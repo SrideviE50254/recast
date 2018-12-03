061d1130b3de96a0f267f013292c52784e6298f7
admin <sunilhari9@gmail.com>
2018-12-03 18:58:44 +0530

    Committed

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 5000
+express()
+    .use(bodyParser.json())    
+    .get('/', (req, res) => {
+        res.send({
+            replies: [{
+                type: 'text',
+                content: 'Welcome to Itelli Bot'
+    }]
+        })
+    })
+    .post('/CreateSO', (req, res) => {
+
+        let materialNo = req.body.conversation.memory.MatNo.value.toString().toUpperCase()
+        let qty = req.body.conversation.memory.Qty.value;
+        res.send({
+            replies: [{
+                type: 'text',
+                content: 'Created Order for Material ' + materialNo + 'with Qty ' + qty
+    }],
+            conversation: {
+                memory: {
+                    key: 'value'
+                }
+            }
+        })
+    })
+
+
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
