06626014e4630ef9b620eddaa50c6ee27739d269
Sridevi <sridevi.268@gmail.com>
2018-12-28 19:16:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ express().use(bodyParser.json()).get('/', (req, res) => {
         res.send({
     replies: [{
       type: 'text',
-      content: "C",
+      content: 'C',
     }],
     conversation: {
       memory: { key: 'value' }
